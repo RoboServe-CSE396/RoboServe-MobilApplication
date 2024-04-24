0cebff6b9c60ea9ddcc389c9ec8b947425492676
reveha <sahinabdullah444@gmail.com>
2024-04-24 03:44:12 +0300

    Database'ye 50 adet yiyecek icecek girildi. Veriler cekildi main menuye aktarildi. Duzeltmeler yapildi.

--- lib/food_item.dart
+++
@@ -1,9 +0,0 @@
-// food_item.dart
-
-class FoodItem {
-  final String name;
-  final double price;
-  final String imagePath; // Add the imagePath property
-
-  FoodItem(this.name, this.imagePath, this.price);
-}
